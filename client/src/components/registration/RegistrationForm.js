deb2322d47f8a9c3a6ba5e2a7085ff9f352cf507
Aaron Alexius <aaronalexius@gmail.com>
2022-05-20 14:06:38 -0400

    Add userName to user object and integrate into TopBar

--- client/src/components/registration/RegistrationForm.js
+++ client/src/components/registration/RegistrationForm.js
@@ -5,6 +5,7 @@ import config from "../../config";
 const RegistrationForm = () => {
   const [userPayload, setUserPayload] = useState({
     email: "",
+    userName: "",
     password: "",
     passwordConfirmation: "",
   });
@@ -15,13 +16,20 @@ const RegistrationForm = () => {
 
   const validateInput = (payload) => {
     setErrors({});
-    const { email, password, passwordConfirmation } = payload;
-    const emailRegexp = config.validation.email.regexp;
+    const { email, userName, password, passwordConfirmation } = payload;
+    const emailRegexp = config.validation.email.regexp.emailRegex;
     let newErrors = {};
     if (!email.match(emailRegexp)) {
       newErrors = {
         ...newErrors,
         email: "is invalid",
+      };
+    }
+
+    if (!userName.length > 2 || !userName.length < 13) {
+      newErrors = {
+        ...newErrors,
+        userName: "is invalid",
       };
     }
 
@@ -94,6 +102,18 @@ const RegistrationForm = () => {
             Email
             <input type="text" name="email" value={userPayload.email} onChange={onInputChange} />
             <FormError error={errors.email} />
+          </label>
+        </div>
+        <div>
+        <label>
+            User Name
+            <input
+              type="text"
+              name="userName"
+              value={userPayload.userName}
+              onChange={onInputChange}
+            />
+            <FormError error={errors.password} />
           </label>
         </div>
         <div>
